28f2966a2389b2d78e8a40d2b50b20e08cfa09a5
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-02-06 11:24:58 +0700

    refactor: add new page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import AboutIndex from '@/components/page/about/AboutIndex';
+
+export type PageProps = {};
+
+const Page = ({}: PageProps): React.ReactElement => <AboutIndex />;
+
+export default Page;
---
+++ src/components/page/about/AboutIndex.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export type AboutIndexProps = {};
+
+const AboutIndex = ({}: AboutIndexProps): React.ReactElement => {
+    return <>
+        ABOUT US
+    </>;
+};
+
+export default AboutIndex;
