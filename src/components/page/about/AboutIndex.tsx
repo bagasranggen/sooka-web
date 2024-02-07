e8049a3e4a4281eab7386fe26d1cee2a322b7432
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-02-07 16:00:14 +0700

    build: ad pages

--- src/components/page/about/AboutIndex.tsx
+++ src/components/page/about/AboutIndex.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import { Container } from 'react-bootstrap';
+import Button from '@/components/common/button/Button';
+
 export type AboutIndexProps = {};
 
 const AboutIndex = ({}: AboutIndexProps): React.ReactElement => {
+    // console.log('test');
+    // console.log(COMMON_ANIMATIONS.BUTTON.RIPPLE);
+
     return <>
-        ABOUT US
+        <Container className="mt-10">
+            <Button
+                type="anchor"
+                variant="ripple"
+                color="light"
+                href="/">TEST</Button>
+            ABOUT US
+        </Container>
     </>;
 };
 
--- src/components/page/productListing/ProductListingIndex.tsx
+++ src/components/page/productListing/ProductListingIndex.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 export type ProductListingIndexProps = {};
 
 const ProductListingIndex = ({}: ProductListingIndexProps): React.ReactElement => (
-    <>PRODUCT LISTING INDEX</>
+    <>
+        <section className="mt-10">
+            <Container>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci distinctio ex ipsum porro quis quod quos unde? At consequuntur corporis deleniti
+                    dolores enim impedit minima quis tenetur unde voluptatem. Ab, consectetur fuga impedit ipsam, iure mollitia nemo nisi nobis nulla odio officiis
+                    perferendis qui quidem quos ratione sapiente veritatis! Ab aliquam aliquid amet aspernatur at culpa delectus dicta dolor ea eius enim expedita fuga id
+                    incidunt mollitia omnis perspiciatis quasi, qui quidem quis repellat saepe sequi soluta totam vel velit voluptatem? Aliquid architecto aspernatur at
+                    aut commodi doloremque dolores, doloribus esse expedita facere fuga id libero, magnam nobis, nostrum omnis quae quia quo rem repellat sapiente sequi
+                    soluta suscipit tempore vel velit voluptates. Ab architecto culpa dolore dolorem doloribus ea, eligendi excepturi hic illum laudantium modi mollitia
+                    nisi non nulla numquam obcaecati officia quaerat quas qui quibusdam repellendus reprehenderit rerum sit ullam vitae voluptas voluptate? Accusantium
+                    libero officia quam quibusdam ratione, voluptates? Ad adipisci aspernatur atque autem blanditiis cum cupiditate debitis deserunt dignissimos dolor
+                    dolores fuga in incidunt inventore ipsum libero magni maiores minus modi molestiae nam natus nemo nisi non odio officiis perferendis perspiciatis
+                    provident quas quasi quisquam quo quos, reiciendis reprehenderit similique velit veniam! Accusamus, dolores, laborum. Atque, aut autem consequatur
+                    dolorem earum eos est eveniet excepturi explicabo hic incidunt inventore ipsa, ipsum itaque iusto labore laboriosam magnam magni maiores modi non
+                    nulla optio pariatur quos sapiente sit veniam voluptas? A ab aliquam aperiam asperiores autem blanditiis cumque deleniti deserunt dolorum eaque
+                    eligendi eum ex exercitationem fugiat incidunt iure laboriosam laborum laudantium magnam maiores maxime nemo neque odio omnis perferendis perspiciatis
+                    placeat provident quaerat quisquam repellendus repudiandae rerum sequi soluta, suscipit, tenetur vero voluptas. Ad amet aperiam debitis delectus dolor
+                    dolorum eos, error esse id maxime modi odio perferendis perspiciatis placeat qui quidem quod reiciendis rerum tenetur voluptatem.</p>
+            </Container>
+        </section>
+        
+        <section className="mt-10">
+            <Container>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci distinctio ex ipsum porro quis quod quos unde? At consequuntur corporis deleniti
+                    dolores enim impedit minima quis tenetur unde voluptatem. Ab, consectetur fuga impedit ipsam, iure mollitia nemo nisi nobis nulla odio officiis
+                    perferendis qui quidem quos ratione sapiente veritatis! Ab aliquam aliquid amet aspernatur at culpa delectus dicta dolor ea eius enim expedita fuga id
+                    incidunt mollitia omnis perspiciatis quasi, qui quidem quis repellat saepe sequi soluta totam vel velit voluptatem? Aliquid architecto aspernatur at
+                    aut commodi doloremque dolores, doloribus esse expedita facere fuga id libero, magnam nobis, nostrum omnis quae quia quo rem repellat sapiente sequi
+                    soluta suscipit tempore vel velit voluptates. Ab architecto culpa dolore dolorem doloribus ea, eligendi excepturi hic illum laudantium modi mollitia
+                    nisi non nulla numquam obcaecati officia quaerat quas qui quibusdam repellendus reprehenderit rerum sit ullam vitae voluptas voluptate? Accusantium
+                    libero officia quam quibusdam ratione, voluptates? Ad adipisci aspernatur atque autem blanditiis cum cupiditate debitis deserunt dignissimos dolor
+                    dolores fuga in incidunt inventore ipsum libero magni maiores minus modi molestiae nam natus nemo nisi non odio officiis perferendis perspiciatis
+                    provident quas quasi quisquam quo quos, reiciendis reprehenderit similique velit veniam! Accusamus, dolores, laborum. Atque, aut autem consequatur
+                    dolorem earum eos est eveniet excepturi explicabo hic incidunt inventore ipsa, ipsum itaque iusto labore laboriosam magnam magni maiores modi non
+                    nulla optio pariatur quos sapiente sit veniam voluptas? A ab aliquam aperiam asperiores autem blanditiis cumque deleniti deserunt dolorum eaque
+                    eligendi eum ex exercitationem fugiat incidunt iure laboriosam laborum laudantium magnam maiores maxime nemo neque odio omnis perferendis perspiciatis
+                    placeat provident quaerat quisquam repellendus repudiandae rerum sequi soluta, suscipit, tenetur vero voluptas. Ad amet aperiam debitis delectus dolor
+                    dolorum eos, error esse id maxime modi odio perferendis perspiciatis placeat qui quidem quod reiciendis rerum tenetur voluptatem.</p>
+            </Container>
+        </section>
+    </>
 );
 
 export default ProductListingIndex;
