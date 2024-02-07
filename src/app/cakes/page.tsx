7758fae2ba42e7ba1941f54e9026201d993fe273
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-02-07 11:13:46 +0700

    build: add product listing page

---
+++ src/app/cakes/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProductListingIndex from '@/components/page/productListing/ProductListingIndex';
+
+export type PageProps = {};
+
+const Page = ({}: PageProps): React.ReactElement => <ProductListingIndex />;
+
+export default Page;
---
+++ src/components/page/productListing/ProductListingIndex.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export type ProductListingIndexProps = {};
+
+const ProductListingIndex = ({}: ProductListingIndexProps): React.ReactElement => (
+    <>PRODUCT LISTING INDEX</>
+);
+
+export default ProductListingIndex;
