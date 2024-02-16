3b06b0fc37f160ad01fa145e0950c7c124f020f3
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-02-16 13:40:43 +0700

    fix: update utility to get better image from google drive

--- src/libs/utils/getGoogleDriveImage.ts
+++ src/libs/utils/getGoogleDriveImage.ts
@@ -1,5 +1,5 @@
 export const getGoogleDriveImage = (url: string) => {
     const split = new URL(url).pathname.split('/');
 
-    return 'https://drive.google.com/thumbnail?id=' + split[3];
+    return 'https://drive.google.com/uc?id=' + split[3];
 };
