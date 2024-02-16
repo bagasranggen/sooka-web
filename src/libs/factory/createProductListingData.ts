11495ae41016c56d461d55cc091e008bdcd53035
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-02-16 13:41:13 +0700

    refactor: update product listing factory

--- src/libs/factory/createProductListingData.ts
+++ src/libs/factory/createProductListingData.ts
@@ -1,19 +1,12 @@
-import { CardImageItemProps } from '@/components/common/card/cardImage/CardImage';
-import { createGoogleDriveImage } from '@/libs/factory/createGoogleDriveImage';
+import type { CardImageItemProps } from '@/components/common/card/cardImage/CardImage';
+import { createGoogleDriveImage } from '@/libs/factory';
 
-export type ProductListingDataProps = {
-    imageDesktop: string;
-    imageDesktopSize: string;
-    imageTablet: string;
-    imageTabletSize: string;
-    imageMobile: string;
-    imageMobileSize: string;
-} & CardImageItemProps;
+export type ProductListingDataProps = Record<'imageDesktop' | 'imageDesktopSize' | 'imageMobile' | 'imageMobileSize', string> & CardImageItemProps;
 
 export const createProductListingData = (datum: ProductListingDataProps) => {
     const images = createGoogleDriveImage({
-        imageSources: [ datum.imageDesktop, datum.imageTablet, datum.imageMobile ],
-        imageSizes: [ JSON.parse(datum.imageDesktopSize), JSON.parse(datum.imageTabletSize), JSON.parse(datum.imageMobileSize) ],
+        imageSources: [ datum.imageDesktop, datum.imageMobile ],
+        imageSizes: [ JSON.parse(datum.imageDesktopSize), JSON.parse(datum.imageMobileSize) ],
         alt: datum.name,
     });
 
