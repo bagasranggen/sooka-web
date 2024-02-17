fa8521f5d4cde0349d7e08ce42a65b267daca901
Bagas Ranggen <bagas.ranggen@gmail.com>
2024-02-17 09:25:24 +0700

    build: temp disable integrated data

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -11,30 +11,31 @@ import ProductListingIndex from '@/components/page/productListing/ProductListing
 export type PageProps = DynamicPageProps;
 
 export const generateStaticParams = async () => {
-    const { data: { data } } = await axiosClient().get(GOOGLE_SPREADSHEET_VARIANT.CATEGORIES);
+    // const { data: { data } } = await axiosClient().get(GOOGLE_SPREADSHEET_VARIANT.CATEGORIES);
 
-    return data.map((datum: FilterProductItemProps) => datum.slug);
+    // return data.map((datum: FilterProductItemProps) => datum.slug);
+    return [ 'cakes', 'drinks', 'hampers' ];
 };
 
 export const generateMetadata = async ({ params }: PageProps) => {
-    const { data: { data: page } } = await axiosClient().get(GOOGLE_SPREADSHEET_VARIANT.PAGES + `/${params.slug}`);
+    // const { data: { data: page } } = await axiosClient().get(GOOGLE_SPREADSHEET_VARIANT.PAGES + `/${params.slug}`);
 
-    if (!page) return notFound();
+    // if (!page) return notFound();
 
     return {
-        title: page.title,
+        title: 'page.title',
     };
 };
 
 const Page = async ({ params }: PageProps): Promise<React.ReactElement> => {
-    const { data: { data: page } } = await axiosClient().get(GOOGLE_SPREADSHEET_VARIANT.PAGES + `/${params.slug}`);
+    // const { data: { data: page } } = await axiosClient().get(GOOGLE_SPREADSHEET_VARIANT.PAGES + `/${params.slug}`);
     // const { data: { data: categories } } = await axiosClient().get(GOOGLE_SPREADSHEET_VARIANT.CATEGORIES);
-    const { data: { data: products } } = await axiosClient().get(GOOGLE_SPREADSHEET_VARIANT.PRODUCT_LISTING + `/${params.slug}`);
+    // const { data: { data: products } } = await axiosClient().get(GOOGLE_SPREADSHEET_VARIANT.PRODUCT_LISTING + `/${params.slug}`);
 
     return <ProductListingIndex
-        page={page}
+        page={{ page: { title: 'title', shortDescription: 'description' } } as any}
         entries={{
-            products,
+            products: [],
             categories: [],
         }} />;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import { axiosClient } from '@/libs/fetcher';
 import HomepageIndex from '@/components/page/homepage/HomepageIndex';
 
 const Home = async () => {
-    const { data: { data } } = await axiosClient().get('homepage');
+    // const { data: { data } } = await axiosClient().get('homepage');
 
     // console.log(data.carousel);
 
     return <HomepageIndex
         entries={{
-            carousel: data.carousel
+            carousel: []
         }} />;
 };
 
