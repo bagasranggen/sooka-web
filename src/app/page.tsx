e2555a5523ef04197d0d03fdc4ad01fa54bf5def
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-03-07 16:53:46 +0700

    feature: init supabase connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { axiosClient } from '@/libs/fetcher';
+import { axiosClient, supabaseClient } from '@/libs/fetcher';
 
 import HomepageIndex from '@/components/page/homepage/HomepageIndex';
 
@@ -6,6 +6,10 @@ const Home = async () => {
     const {
         data: { data },
     } = await axiosClient().get('homepage');
+    const supabase = supabaseClient();
+    const { data: notes } = await supabase.from('navigation').select();
+
+    console.log('homepage', notes);
 
     // console.log(data.carousel);
     return <HomepageIndex entries={{ carousel: data.carousel }} />;
