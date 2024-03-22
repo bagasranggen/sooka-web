3020a1fad676b94b5bd27b7bdf4e9dd919ca8ea5
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-03-22 14:33:08 +0700

    style: update login form ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import UserLoginIndex from '@/components/page/userLogin/UserLoginIndex';
 
 export type PageProps = {};
 
 const Page = ({}: PageProps): React.ReactElement => {
-    return <UserLoginIndex />;
+    return (
+        <Suspense>
+            <UserLoginIndex />
+        </Suspense>
+    );
 };
 
 export default Page;
--- src/components/common/form/formUserLogin/FormUserLogin.tsx
+++ src/components/common/form/formUserLogin/FormUserLogin.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import { FORM_VARIANTS } from '@/libs/form';
+
 import Input from '@/components/common/input/Input';
 import Button from '@/components/common/button/Button';
+import Icon from '@/components/common/icon/Icon';
 
 export type FormUserLoginProps = {
     variant: typeof FORM_VARIANTS.USER_LOGIN;
@@ -10,40 +13,50 @@ export type FormUserLoginProps = {
 
 const FormUserLogin = ({ events }: FormUserLoginProps): React.ReactElement => (
     <form {...events}>
-        <div className="row">
-            <div className="col-md-6">
+        <div className="mb-6 text-center">
+            <Icon
+                variant="sooka"
+                color="primary"
+                style={{ maxWidth: '25rem' }}
+            />
+        </div>
+
+        <div className="row justify-content-center text-center">
+            <div className="col-md-8 col-xl-6">
                 <Input
                     variant="floating"
+                    options={{
+                        align: 'center',
+                    }}
                     input={{
                         id: 'email',
                         type: 'email',
-                        label: 'label',
+                        label: 'Email',
                     }}
                 />
+
                 <Input
                     variant="floating"
+                    className="mt-3"
+                    options={{
+                        align: 'center',
+                    }}
                     input={{
                         id: 'password',
                         type: 'password',
-                        label: 'label',
+                        label: 'Password',
                     }}
                 />
+
+                <Button
+                    variant="ripple"
+                    color="primary"
+                    className="mx-auto mt-5"
+                    type="submit">
+                    SUBMIT
+                </Button>
             </div>
         </div>
-
-        <Button
-            variant="ripple"
-            color="primary"
-            // color=""
-            type="submit">
-            Submit
-        </Button>
-
-        {/*<button*/}
-        {/*    className="btn"*/}
-        {/*    type="submit">*/}
-        {/*    SUBMIT*/}
-        {/*</button>*/}
     </form>
 );
 export default FormUserLogin;
