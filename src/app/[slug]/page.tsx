4b86eb5bb9f35577a7e095e846e9b0bd84af8cfd
Bagas Ranggen <bagas.ranggen@gmail.com>
2024-04-17 21:36:22 +0700

    refactor: refactor product listing page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,61 +2,22 @@ import React from 'react';
 import { notFound } from 'next/navigation';
 
 import type { DynamicPageProps } from '@/libs/@types';
-import { supabaseServerAction } from '@/libs/fetcher/supabaseServerAction';
-import { createProductListingData, type ProductListingDataProps } from '@/libs/factory';
 
-import type { FilterProductItemProps } from '@/components/common/filter/filterProduct/FilterProduct';
-import ProductListingIndex, {
-    type ProductListingIndexProps,
-} from '@/components/page/productListing/ProductListingIndex';
+import ProductListingIndex from '@/components/page/productListing/ProductListingIndex';
+import ProductListingData from '@/components/page/productListing/ProductListingData';
 
 export type PageProps = DynamicPageProps;
 
-const getPage = async (slug: string): Promise<{ data: ProductListingIndexProps['page'] }> => {
-    const { data } = await supabaseServerAction({
-        variant: 'fetch-find',
-        relation: 'pages',
-        slug,
-    });
+export const generateStaticParams = async ({ params }: PageProps) => {
+    const { path } = await ProductListingData(params.slug as string);
 
-    if (data?.length === 0) return notFound();
-
-    return {
-        data: data ? { title: data[0]?.title, description: data[0]['short_description'] } : ({} as any),
-    };
-};
-
-const getProductListing = async (slug: string) => {
-    const { data } = await supabaseServerAction({
-        variant: 'fetch-filter',
-        relation: 'productListing',
-        filter: {
-            key: 'category',
-            slug,
-        },
-    });
-
-    let productListing: ProductListingDataProps[] = [];
-    if (data && data?.length > 0) {
-        data.map((datum: ProductListingDataProps) => {
-            productListing.push(createProductListingData(datum));
-        });
-    }
-
-    return { data: productListing };
-};
-
-export const generateStaticParams = async () => {
-    const { data } = await supabaseServerAction({
-        variant: 'fetch',
-        relation: 'categories',
-    });
-
-    return (data as any[]).map((datum: FilterProductItemProps) => datum.slug);
+    return path;
 };
 
 export const generateMetadata = async ({ params }: PageProps) => {
-    const { data: page } = await getPage(params.slug as string);
+    const { page } = await ProductListingData(params.slug as string);
+
+    if (!page?.title) return notFound();
 
     return {
         title: page.title,
@@ -64,8 +25,7 @@ export const generateMetadata = async ({ params }: PageProps) => {
 };
 
 const Page = async ({ params }: PageProps): Promise<React.ReactElement> => {
-    const { data: page } = await getPage(params.slug as string);
-    const { data: products } = await getProductListing(params.slug as string);
+    const { page, products } = await ProductListingData(params.slug as string);
 
     return (
         <ProductListingIndex
---
+++ src/components/page/productListing/ProductListingData.ts
@@ -0,0 +1,56 @@
+import { supabaseServerAction } from '@/libs/fetcher';
+import { createProductListingData, ProductListingDataProps } from '@/libs/factory';
+
+import type { ProductListingIndexProps } from '@/components/page/productListing/ProductListingIndex';
+import type { FilterProductItemProps } from '@/components/common/filter/filterProduct/FilterProduct';
+
+const getPage = async (slug: string): Promise<{ data: ProductListingIndexProps['page'] }> => {
+    const { data } = await supabaseServerAction({
+        variant: 'fetch-find',
+        relation: 'pages',
+        slug,
+    });
+
+    return {
+        data: data ? { title: data[0]?.title, description: data[0]?.['short_description'] } : ({} as any),
+    };
+};
+
+const getProductListing = async (slug: string) => {
+    const { data } = await supabaseServerAction({
+        variant: 'fetch-filter',
+        relation: 'productListing',
+        filter: {
+            key: 'category',
+            slug,
+        },
+    });
+
+    let productListing: ProductListingDataProps[] = [];
+    if (data && data?.length > 0) {
+        data.map((datum: ProductListingDataProps) => {
+            productListing.push(createProductListingData(datum));
+        });
+    }
+
+    return { data: productListing };
+};
+
+const getPath = async () => {
+    const { data } = await supabaseServerAction({
+        variant: 'fetch',
+        relation: 'categories',
+    });
+
+    return { data: (data as any[]).map((datum: FilterProductItemProps) => datum.slug) };
+};
+
+const productListingData = async (slug: string) => {
+    const { data: page } = await getPage(slug);
+    const { data: products } = await getProductListing(slug);
+    const { data: path } = await getPath();
+
+    return { page, products, path };
+};
+
+export default productListingData;
