dbb8335b8782190fd0eebddb4de1fc265882160b
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-04-18 15:13:42 +0700

    feature: add slider

--- src/components/page/productDetail/ProductDetailIndex.tsx
+++ src/components/page/productDetail/ProductDetailIndex.tsx
@@ -5,11 +5,12 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 import { getWhatsappEncoded } from '@/libs/utils';
 
-import Picture, { PictureItemProps } from '@/components/common/picture/Picture';
+import type { PictureItemProps } from '@/components/common/picture/Picture';
 import Button, { ButtonGroup } from '@/components/common/button/Button';
+import Slider from '@/components/common/slider/Slider';
 
 export type ProductDetailProps = {
-    title: string;
+    title?: string;
     description?: string;
     price: string;
     slides: Array<PictureItemProps[]>;
@@ -19,44 +20,36 @@ const ProductDetailIndex = ({ title, description, price, slides }: ProductDetail
     return (
         <section className="ts--margin py-8 product-detail">
             <Container>
-                <Row className="justify-content-center">
-                    <Col lg={11}>
-                        <Row className="justify-content-between gy-4">
-                            <Col lg={6}>
-                                <div className="product-detail__image">
-                                    {slides.map((slide: any, i: number) => (
-                                        <Picture
-                                            key={i}
-                                            items={slide}
-                                        />
-                                    ))}
-                                </div>
-                            </Col>
-                            <Col lg={5}>
-                                <div className="product-detail__content">
-                                    <div className="product-detail__title">
-                                        <h1>{title}</h1>
-                                        <h2>
-                                            <span>Rp</span>
-                                            {price}
-                                        </h2>
-                                    </div>
-                                    {description && (
-                                        <div className="product-detail__description">
-                                            {ReactHtmlParser(description)}
-                                        </div>
-                                    )}
-                                    <ButtonGroup className="d-flex mt-3">
-                                        <Button
-                                            variant="rounded"
-                                            type="anchor"
-                                            href={getWhatsappEncoded(title)}>
-                                            Order
-                                        </Button>
-                                    </ButtonGroup>
-                                </div>
-                            </Col>
-                        </Row>
+                <Row className="justify-content-between gy-4">
+                    <Col lg={6}>
+                        <div className="product-detail__image">
+                            <Slider
+                                variant="vertical"
+                                items={slides}
+                            />
+                        </div>
+                    </Col>
+                    <Col lg={5}>
+                        <div className="product-detail__content">
+                            <div className="product-detail__title">
+                                <h1>{title}</h1>
+                                <h2>
+                                    <span>Rp</span>
+                                    {price}
+                                </h2>
+                            </div>
+                            {description && (
+                                <div className="product-detail__description">{ReactHtmlParser(description)}</div>
+                            )}
+                            <ButtonGroup className="d-flex mt-3">
+                                <Button
+                                    variant="rounded"
+                                    type="anchor"
+                                    href={getWhatsappEncoded(title ?? '')}>
+                                    Order
+                                </Button>
+                            </ButtonGroup>
+                        </div>
                     </Col>
                 </Row>
             </Container>
