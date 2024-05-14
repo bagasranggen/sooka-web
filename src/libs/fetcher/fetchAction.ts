bb50f8fb23ceb0c524d143f82876f2816fd13f24
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2024-05-14 10:28:57 +0700

    refactor: update use on fetcher action

--- src/libs/fetcher/fetchAction.ts
+++ src/libs/fetcher/fetchAction.ts
@@ -1,20 +1,40 @@
+export type FetchActionRevalidateItemProps = {
+    url: string;
+    type?: 'page' | 'layout';
+};
+
 export type FetchActionRevalidateProps = {
     variant: 'revalidate';
-    path: string;
-    type?: 'page' | 'layout';
+    path: FetchActionRevalidateItemProps | FetchActionRevalidateItemProps[];
 };
 
 export type FetchActionProps = FetchActionRevalidateProps;
 
 export const fetchAction = async (props: FetchActionProps) => {
-    let url = process.env.NEXT_PUBLIC_API_URL ?? '';
+    let baseUrl = process.env.NEXT_PUBLIC_API_URL ?? '';
+    let urls = [];
 
     switch (props.variant) {
         case 'revalidate':
-            url += `revalidate?path=${props.path}`;
-            if (props.type) url += `&type=${props.type}`;
+            baseUrl += `revalidate`;
+
+            if (Array.isArray(props.path)) {
+                props.path.map((item: FetchActionRevalidateItemProps) => {
+                    let temp = `${baseUrl}?path=${item.url}`;
+
+                    if (item?.type) temp += `&type=${item.type}`;
+                    urls.push(temp);
+                });
+            } else {
+                baseUrl += `?path=${props.path.url}`;
+                if (props.path.type) baseUrl += `&type=${props.path.type}`;
+                urls.push(baseUrl);
+            }
+
             break;
     }
 
-    fetch(url);
+    urls.map(async (item: string) => {
+        await fetch(item);
+    });
 };
